37c1a2d054e65dee548b7f7fde8458d34d5e1238
Ic3 <chitrathep.c@codium.co>
2021-01-21 12:05:46 +0700

    🔨 Add script restart service

--- init-systemd-glances.sh
+++ init-systemd-glances.sh
@@ -3,3 +3,4 @@ cp ./glances.service /etc/systemd/system/glances.service
 systemctl enable glances.service
 systemctl start glances.service
 systemctl restart glances.service
+systemctl status glances.service
